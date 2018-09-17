783f70306525b924e4ce1733d49775982c8b27dd
aarashtaheri <aarash.taheri@gmail.com>
2018-09-17 12:53:07 +0200

    initial commit

---
+++ github-resume/src/main/java/com/arash/githubresume/GithubResumeApplication.java
@@ -0,0 +1,12 @@
+package com.arash.githubresume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubResumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubResumeApplication.class, args);
+	}
+}
